652a979515ee5219274ded8da86220f4461720d6
Michael Chirlin <michael.chirlin@appian.com>
2015-01-17 10:53:35 +1100

    Adding contact page.

---
+++ contactus.php
@@ -0,0 +1,24 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/navigation.php"); ?>
+
+  <div class="row clearfix">
+		<div class="col-md-8 column">
+			<img alt="140x140" src="http://lorempixel.com/140/140/">
+		</div>
+		<div class="col-md-4 column">
+			<span class="label label-warning">Facebook</span>
+			<p>
+		    <a href="www.facebook.com/ColonyUltimate">Colony Ultimate</a>
+			</p>
+			<span class="label label-warning">Twitter</span>
+			<p>
+				<a href="www.twitter.com/colonyultimate">colonyultimate</a>
+			</p>
+			<span class="label label-warning">Email</span>
+			<p>
+				mark.evans2212@gmail.com
+			</p>
+		</div>
+	</div>
+	
+<?php include("includes/footer.php"); ?>
