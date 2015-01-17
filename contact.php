d3aae73f76b879cb0a0d6217ada55852760e6ee9
Michael Chirlin <michael.chirlin@appian.com>
2015-01-17 11:12:33 +1100

    Styling changes for contact page

--- contact.php
+++ contact.php
@@ -2,23 +2,32 @@
 <?php include("includes/navigation.php"); ?>
 
   <div class="row clearfix">
-		<div class="col-md-8 column">
-			<img alt="140x140" src="http://colonyultimate.com/photos/contactTeam.jpg">
-		</div>
-		<div class="col-md-4 column">
-			<span class="label label-warning">Facebook</span>
-			<p>
-		    <a href="www.facebook.com/ColonyUltimate">Colony Ultimate</a>
-			</p>
-			<span class="label label-warning">Twitter</span>
-			<p>
-				<a href="www.twitter.com/colonyultimate">colonyultimate</a>
-			</p>
-			<span class="label label-warning">Email</span>
-			<p>
-				mark.evans2212@gmail.com
-			</p>
-		</div>
-	</div>
-	
+    <div class="col-md-12 column">
+      <div class="jumbotron">
+        <div class="row clearfix">
+          <h2>Contact Us</h2> 
+        </div>
+        <div class="row clearfix">   
+          <div class="col-md-8 column">   
+            <img class="img-responsive" src="http://colonyultimate.com/photos/contactTeam.jpg">
+          </div>
+          <div class="col-md-4 column">
+            <span class="label label-danger">EMAIL</span>
+            <p>
+              mark.evans2212@gmail.com
+            </p>
+            <span class="label label-danger">FACEBOOK</span>
+            <p>
+              <a href="http://facebook.com/ColonyUltimate">Colony Ultimate</a>
+            </p>
+            <span class="label label-danger">TWITTER</span>
+            <p>
+              <a href="http://twitter.com/colonyultimate">colonyultimate</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 <?php include("includes/footer.php"); ?>
