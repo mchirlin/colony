76e15b95bbc54f9fe3394b4eae2c8a8cd8e24573
Michael Chirlin <michael.chirlin@appian.com>
2015-01-21 22:15:35 +1100

    Testing player bio

--- includes/player_bio.php
+++ includes/player_bio.php
@@ -1,4 +1,11 @@
 <div class="player">
-  <?php echo $player[1]; ?>
+  <div class="number">
+    <img src="http://colonyultimate.com/photos/
+    <?php
+      if (file_exists('http://colonyultimate/photos' + $player[3] + '.png')) echo 'generic';
+      else echo str_replace(" ", "-", $player[3]);
+    ?>.png" />
+  </div>
+  <div class="number"><?php echo $player[3]; ?></div>
+  <div class="name"><?php echo $player[1]; ?></div>
 </div>
-<br />
