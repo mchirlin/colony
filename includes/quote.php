12d967e257557365e112f665a971c06546075c35
Michael Chirlin <michael.chirlin@appian.com>
2015-01-22 09:57:06 +1100

    Another bug

--- includes/quote.php
+++ includes/quote.php
@@ -1,13 +1,13 @@
 <blockquote>
 	<p>
-		<?php echo $email[1] ?>
-	</p> <small><?php echo $email[2] <cite>Email <?php echo $email[0]; ?></cite></small>
+		<?php echo $email[1]; ?>
+	</p> <small><?php echo $email[2]; ?> <cite>Email <?php echo $email[0]; ?></cite></small>
 	
 	<?php 
 	  if($email[3] != "") {
 	     echo "<p>";
 	     echo $email[3];
-	     echo "</p> <small>" . $email[4] . " <cite>Email <?php echo $email[0]; ?></cite></small>";
+	     echo "</p> <small>" . $email[4] . " <cite>Email ". $email[0] . "</cite></small>";
 	   }
 	 ?>
 </blockquote>
