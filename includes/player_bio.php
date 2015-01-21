28c3a9cbe87a6b9a2dc29b8486d46125e72eeb67
Michael Chirlin <michael.chirlin@appian.com>
2015-01-21 22:36:17 +1100

    Changing nophoto

--- includes/player_bio.php
+++ includes/player_bio.php
@@ -1,10 +1,6 @@
 <div class="player">
   <div class="number">
-    <img src="http://colonyultimate.com/photos/
-    <?php
-      if (file_exists('http://colonyultimate/photos' + $player[3] + '.png')) echo 'generic';
-      else echo str_replace(" ", "-", $player[3]);
-    ?>.png" />
+    <img src="http://colonyultimate.com/photos/<?php if (file_exists('http://colonyultimate/photos' + $player[1] + '.png')) echo str_replace(" ", "-", $player[1]); else echo 'nophoto'; ?>.png" />
   </div>
   <div class="number"><?php echo $player[3]; ?></div>
   <div class="name"><?php echo $player[1]; ?></div>
