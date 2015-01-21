e669be3a2179f6f4936cb78be928bc73677456f0
Michael Chirlin <michael.chirlin@appian.com>
2015-01-21 16:23:07 +1100

    Adding sponsor

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,8 @@
     </div>
+    <div id="sponsors">
+      <ul class="sponsors">
+        <li><a href="http://www.zueultimate.com/en/" target="_blank"><img src="http://colonyultimate.com/img/sponsors/zue.png" onmouseover="this.src='http://colonyultimate.com/img/sponsors/zue_hover.png'" onmouseout="this.src='http://colonyultimate.com/img/sponsors/zue.png'"></a></li>
+      </ul>
+    </div>
   </body>
 </html>
