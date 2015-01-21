4f2da1572af385905b810b6e9e62f8acfe51bec3
Michael Chirlin <michael.chirlin@appian.com>
2015-01-21 21:45:47 +1100

    Splitting to pillage and plunder

--- includes/year_roster.php
+++ includes/year_roster.php
@@ -10,62 +10,78 @@
   
   fclose($rosterFile);
 	
-	$oHandlers = array();
-	$oCutters = array();
-	$dHandlers = array();
-	$dCutters = array();
+	$oHandlersPill = array();
+	$oCuttersPill = array();
+	$dHandlersPill = array();
+	$dCuttersPill = array();
+	
+  $oHandlersPlun = array();
+	$oCuttersPlun = array();
+	$dHandlersPlun = array();
+	$dCuttersPlun = array();
 	
 	foreach ($roster as $key => $value) {
 	  switch ($value[5]) {
 	    case 'OH':
-	      array_push($oHandlers, $value);
+	      if($value[2] == 'Pillage') array_push($oHandlersPill, $value);
+	      else if($value[2] == 'Pillage') array_push($oHandlersPlun, $value);
 	      break;
 	    case 'OC':
-	      array_push($oCutters, $value);
+	      if($value[2] == 'Pillage') array_push($oCuttersPill, $value);
+	      else if($value[2] == 'Pillage') array_push($oCuttersPlun, $value);
 	      break;
 	    case 'DH':
-	      array_push($dHandlers, $value);
+	      if($value[2] == 'Pillage') array_push($dHandlersPill, $value);
+	      else if($value[2] == 'Pillage') array_push($dHandlersPlun, $value);
 	      break;
 	    case 'DC':
-	      array_push($dCutters, $value);
+	      if($value[2] == 'Pillage') array_push($dCuttersPill, $value);
+	      else if($value[2] == 'Pillage') array_push($dCuttersPlun, $value);
 	      break;
 	  }
 	}
 ?>
 
+<div class="row clearfix">
+  <div class="col-md-6 column">
+  	<img class="img-responsive" src="http://colonyultimate.com/img/pillage.png">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title" contenteditable="true">Offensive Handlers</h3>
+      </div>
+      <div class="panel-body" contenteditable="true">
+        <?php foreach($oHandlersPill as $player) include("includes/player_bio.php"); ?>
+      </div>
+    </div>
 
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title" contenteditable="true">Offensive Handlers</h3>
-  </div>
-  <div class="panel-body" contenteditable="true">
-    <?php foreach($oHandlers as $player) include("includes/player_bio.php"); ?>
-  </div>
-</div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title" contenteditable="true">Offensive Cutters</h3>
+      </div>
+      <div class="panel-body" contenteditable="true">
+        <?php foreach($oCuttersPill as $player) include("includes/player_bio.php"); ?>
+      </div>
+    </div>
 
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title" contenteditable="true">Offensive Cutters</h3>
-  </div>
-  <div class="panel-body" contenteditable="true">
-    <?php foreach($oCutters as $player) include("includes/player_bio.php"); ?>
-  </div>
-</div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title" contenteditable="true">Defensive Handlers</h3>
+      </div>
+      <div class="panel-body" contenteditable="true">
+        <?php foreach($dHandlersPill as $player) include("includes/player_bio.php"); ?>
+      </div>
+    </div>
 
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title" contenteditable="true">Defensive Handlers</h3>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title" contenteditable="true">Defensive Cutters</h3>
+      </div>
+      <div class="panel-body" contenteditable="true">
+        <?php foreach($dCuttersPill as $player) include("includes/player_bio.php"); ?>
+      </div>
+    </div>
   </div>
-  <div class="panel-body" contenteditable="true">
-    <?php foreach($dHandlers as $player) include("includes/player_bio.php"); ?>
+  <div class="col-md-6 column">
+  	<img class="img-responsive" src="http://colonyultimate.com/img/plunder.png">
   </div>
-</div>
-
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title" contenteditable="true">Defensive Cutters</h3>
-  </div>
-  <div class="panel-body" contenteditable="true">
-    <?php foreach($dCutters as $player) include("includes/player_bio.php"); ?>
-  </div>
-</div>
+</div
