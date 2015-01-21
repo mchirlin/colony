764304ace1dce96d68ecf98e6bcbc2cf1a18f8bc
Michael Chirlin <michael.chirlin@appian.com>
2015-01-21 16:27:52 +1100

    Changing sponsor style

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
     </div>
-    <div id="sponsors">
+    <div class="jumbotron">
       <ul class="sponsors">
         <li><a href="http://www.zueultimate.com/en/" target="_blank"><img src="http://colonyultimate.com/img/sponsors/zue.png" onmouseover="this.src='http://colonyultimate.com/img/sponsors/zue_hover.png'" onmouseout="this.src='http://colonyultimate.com/img/sponsors/zue.png'"></a></li>
       </ul>
