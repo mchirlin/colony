c5853b0f96d12f52d8a60c9c3d844a90886cd4d4
Michael Chirlin <michael.chirlin@appian.com>
2015-01-21 22:53:25 +1100

    Few updates

--- includes/player_bio.php
+++ includes/player_bio.php
@@ -1,7 +1,7 @@
 <div class="player">
-  <div class="number">
-    <img src="http://colonyultimate.com/photos/<?php if (file_exists('http://colonyultimate/photos' + $player[1] + '.png')) echo str_replace(" ", "-", $player[1]); else echo 'nophoto'; ?>.png" />
+  <div class="photo">
+    <img src="http://colonyultimate.com/photos/<?php if (file_exists('http://colonyultimate/photos' + $player[1] + '.png')) echo str_replace(" ", "-", $player[1]); else if($player[2] == "Pillage") echo 'nophoto'; else echo 'nophoto2'; ?>.png" />
   </div>
-  <div class="number"><?php echo $player[3]; ?></div>
+  <div class="number"><a href="#"><?php echo $player[3]; ?></a></div>
   <div class="name"><?php echo $player[1]; ?></div>
 </div>
