4e722cb6591dc70ea2049dac5cf67d14cce6c901
Michael Chirlin <michael.chirlin@appian.com>
2015-02-05 00:52:45 +1100

    Making modal bigger

--- team/index.php
+++ team/index.php
@@ -2,8 +2,8 @@
 <?php include("../includes/header.php"); ?>
 <?php include("../includes/navigation.php"); ?>
 
-<div class="modal fade" id="smallModal" tabindex="-1" role="dialog" aria-labelledby="smallModal" aria-hidden="true">
-  <div class="modal-dialog modal-sm">
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="smallModal" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
--- team/player_box.php
+++ team/player_box.php
@@ -1,5 +1,5 @@
 <div class="player">
-  <a href="#" data-toggle="modal" data-target="#smallModal" data-remote="http://colonyultimate.com/player_bio.php?name=<?php echo urlencode($player[1]); ?>">
+  <a href="#" data-toggle="modal" data-target="#modal" data-remote="http://colonyultimate.com/player_bio.php?name=<?php echo urlencode($player[1]); ?>">
     <div class="photo">
       <img src="http://colonyultimate.com/team/photos/<?php if (file_exists('photos/' . strtolower(str_replace(" ", "-", $player[1])) . '.png')) echo strtolower(str_replace(" ", "-", $player[1])); else if($player[2] == "Pillage") echo 'nophoto'; else echo 'nophoto2'; ?>.png" />
     </div>
