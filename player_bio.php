92b29ef463ba669efd5f5dc4fb417893a5be40c2
Michael Chirlin <michael.chirlin@appian.com>
2015-02-05 01:29:40 +1100

    Adding years on team

--- player_bio.php
+++ player_bio.php
@@ -3,13 +3,13 @@
   $name = $_GET["name"];
   
   $rosterFile = fopen("data/roster.csv","r");
-  $roster = array();
+  $years = array();
   
   while(! feof($rosterFile)) {
     $array = fgetcsv($rosterFile);
-    if($array[0] == $year && $array[1] == $name) {
-      $player = $array;
-      break;
+    if($array[1] == $name) {
+      array_push($years, $array[0]);
+      if($array[0] == $year) $player = $array;
     } 
   }
   
@@ -21,8 +21,16 @@
   <h4 class="modal-title" id="myModalLabel"><?php echo $player[1]; ?></h4>
 </div>
 <div class="modal-body">
-  <div class="photo">
-    <img src="http://colonyultimate.com/team/photos/<?php if (file_exists('photos/' . strtolower(str_replace(" ", "-", $player[1])) . '.png')) echo strtolower(str_replace(" ", "-", $player[1])); else if($player[2] == "Pillage") echo 'nophoto'; else echo 'nophoto2'; ?>.png" />
+<div class="row clearfix">
+  <div class="col-md-4 column">
+  	<div class="photo">
+      <img src="http://colonyultimate.com/team/photos/<?php if (file_exists('photos/' . strtolower(str_replace(" ", "-", $player[1])) . '.png')) echo strtolower(str_replace(" ", "-", $player[1])); else if($player[2] == "Pillage") echo 'nophoto'; else echo 'nophoto2'; ?>.png" />
+    </div>
+  </div>
+  <div class="col-md-8 column">
+    Number: <?php echo $player[3]; ?><br />
+    Bio:<br />
+    Years on team: <?php echo implode(', ', $years); ?>
   </div>
 </div>
 <div class="modal-footer">
